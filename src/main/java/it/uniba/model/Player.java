64ddbb60788ac905c70dc5177597926d689c993b
lucazeverino <lucazeverino@gmail.com>
2020-06-02 18:32:06 +0200

    Aggiunta lista "Inventario" nella classe Player con metodi d'accesso

--- src/main/java/it/uniba/model/Player.java
+++ src/main/java/it/uniba/model/Player.java
@@ -1,10 +1,20 @@
 package it.uniba.model;
 
+import java.util.ArrayList;
+
 public class Player {
 
-    public Player() {
-        System.out.println("*** Player");
+    private static ArrayList<Item> inventory;
+
+    public static void addItemInventory(Item item) {
+        inventory.add(item);
     }
 
+    public static void removeItemInventory(Item item) {
+        inventory.remove(item);
+    }
 
+    public static ArrayList<Item> getItemsList () {
+        return inventory;
+    }
 }
