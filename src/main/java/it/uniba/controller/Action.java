43508acf40c3c03487541ac2fef18b98edd66208
lucazeverino <lucazeverino@gmail.com>
2020-06-10 11:23:43 +0200

    Javadoc Action

--- src/main/java/it/uniba/controller/Action.java
+++ src/main/java/it/uniba/controller/Action.java
@@ -5,10 +5,24 @@ import it.uniba.model.Player;
 
 import java.util.ArrayList;
 
+/**
+ * <p><code>Controller</code></p> La classe <code>Action</code> è una classe statica e contiene tutti i metodi
+ * corrispondenti alle azioni possibili da effettuare in gioco.
+ */
 public final class Action {
+    /**
+     * Crea costruttore privato della classe <code>Action</code>
+     */
     private Action() {
 
     }
+
+    /**
+     * Restituisce una stringa formattata contenente l'intero menu help
+     * con nome dei comandi e relative descrizioni.
+     *
+     * @return string
+     */
     public static String help() {
         String out = "\033[2J\033[H";
         out += "Lista comandi:\n";
@@ -22,6 +36,13 @@ public final class Action {
 
     }
 
+    /**
+     * Imposta la stanza corrente alla stanza iniziale del gioco,
+     * a condizione che si possegga e sia un uso un oggetto specifico.
+     *
+     * @param loader game loader
+     * @return true/false
+     */
     public static boolean home(final GameLoader loader) {
         for (Item item : Player.getItemsList()) {
             if (item.getItemName().equals("gomitolo")) {
@@ -37,6 +58,16 @@ public final class Action {
             }
         return false;
     }
+
+    /**
+     * Se nella stanza corrente c'è un solo item, viene preso e messo nell'inventario anche senza essere esplicitato;
+     * Se ci sono più item, è necessario specificare l'item da prendere.
+     *
+     * @param loader game loader
+     * @param roomId stanza corrente
+     * @param tk lista token input
+     * @return true/false
+     */
     public static boolean pickUpItem(final GameLoader loader, final int roomId, final String[] tk) {
         if (tk.length == 1 && loader.getPlotRooms().get(roomId).getItemsList().size() == 1) {
             Item selI = loader.getPlotRooms().get(roomId).getItemsList().get(0);
@@ -57,6 +88,15 @@ public final class Action {
         }
     }
 
+    /**
+     * Se nell'inventario c'è un solo item, viene lasciato nella stanza corrente anche senza essere esplicitato;
+     * Se ci sono più item, è necessario specificare l'item da lasciare.
+     *
+     * @param loader game loader
+     * @param roomId stanza corrente
+     * @param tk lista token input
+     * @return true/false
+     */
     public static boolean leaveItem(final GameLoader loader, final int roomId, final String[] tk) {
         if (tk.length == 1 && Player.getItemsList().size() == 1) {
             Player.getItemsList().get(0).setUse(false);
@@ -81,6 +121,13 @@ public final class Action {
         }
     }
 
+    /**
+     * Se nell'inventario c'è un solo item, viene settato in uso anche senza essere esplicitato;
+     * Se ci sono più item, è necessario specificare l'item da usare
+     *
+     * @param tk lista token input
+     * @return true/false
+     */
     public static boolean useItem(final String[] tk) {
         if (tk.length == 1 && Player.getItemsList().size() == 1) {
             if (!Player.getItemsList().get(0).getUse()) {
@@ -103,6 +150,15 @@ public final class Action {
             return  false;
         }
     }
+
+    /**
+     * Restituisce una stringa variabile dinamicamente in base all'esito dello scontro
+     * e al nemico
+     *
+     * @param loader game loader
+     * @param roomId stanza corrente
+     * @return string
+     */
     public static String fight(final GameLoader loader, final int roomId) {
         String out = "\033[2J\033[H";
         ArrayList<Item> roomItems = loader.getPlotRooms().get(roomId).getItemsList();
@@ -163,9 +219,6 @@ public final class Action {
      * @return output string
      */
     public static String showInventory() {
-      //  GameLoader loader = new GameLoader();
-      //  Player.addItemInventory(loader.getPlotRooms().get(1).getItemsList().get(0));
-      //  Player.addItemInventory(loader.getPlotRooms().get(3).getItemsList().get(0));
         String out = "\033[2J\033[H";
         if (Player.getItemsList().size() == 0) {
             out += "\n" + "L'inventario è vuoto" + "\n";
@@ -198,7 +251,15 @@ public final class Action {
         }
         return out;
     }
-
+    /**
+     * Controlla che la stanza corrente abbia una exit passata come parametro
+     * e nel caso sia presente compie lo spostamento nella
+     * stanza di destinazione, altrimenti restituisce false
+     *
+     * @param roomId stanza corrente
+     * @param cmd exit di destinazione
+     * @return true/false
+     */
     public static boolean goTo(final int roomId, final String cmd) {
         GameLoader gLoader = new GameLoader();
         if (gLoader.getPlotRooms().get(roomId).getExitRoom(cmd) != null) {
